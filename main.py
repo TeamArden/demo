7e7cc491a57c4e8d33e558f36945348e1cd7f39a
kali <teamarden7@gmail.com>
2023-04-02 22:37:26 +0300

    добавили цикл for

--- main.py
+++ main.py
@@ -3,14 +3,15 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
+a = [1,2,3,4,5,6,7]
 def print_hi(name):
+    for a in name:
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+        print(f'Hi, {a}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi(a)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
